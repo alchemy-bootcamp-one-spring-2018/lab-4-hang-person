d8df21fb3574ba6418fd3ef0576a9956a9150321
rbtprograms <rbt61690@gmail.com>
2018-05-03 14:13:12 -0700

    pulling word out of array not working

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ function letterGuess() {
     var guessed = userGuess.value.trim();
     console.log(guessed);
 }
+
+function getWord(){
+    var choices = document.getElementById('words');
+    var wordChoice = choices[Math.floor(Math.random() * 5)];
+    console.log(wordChoice);
+}
+
+getWord();
