ad21da043860562aa819c6d250b4b224a44edeb8
KLPainter <KLPainter@gmail.com>
2018-05-03 16:58:59 -0700

    end of day Thursday

--- app.js
+++ app.js
@@ -16,38 +16,36 @@ function loadWord() {
     return wordList[randomIndex]
 }
 
-
 var currentWord = loadWord();
+var currentWordArray = currentWord.split ('');
 
-var p = document.getElementById('currentArrayWord');
+var p = document.getElementById('tempViewWord');
 p.textContent = currentWord;
 console.log ('p is:', p);
-
-//document.getElementById('currentArrayWord').textContent = currentWord;
 
 function guess() {
 
     var letterGuess = document.getElementById('guessInput').value;
     console.log ('From guessInput box:', letterGuess);
+
     //if user does not enter a single letter, stop running.
     if(letterGuess.length !== 1) {
         alert('Please enter exactly one letter.');
         return;
     }
-    
-    //var letterIndex = currentWord.indexOf(letterGuess);
-    //console.log ('Found letterGuess at position:', letterIndex);
+
     var startAtIndex = 0;
     var letterIndex;
     var wrongGuesses = [];
-//checking if guess is in word and pushing wrong guesses into array
+    //checking if guess is in word and pushing wrong guesses into array
     if(currentWord.indexOf(letterGuess, startAtIndex) === -1){
         wrongGuesses.push(letterGuess);
         console.log('wrong guesses:', wrongGuesses);
+        // TO DO: add a body part
     }
+    // TO DO: no need to continue
 
-
-//while loop to find guessed letter position
+    //while loop to find guessed letter position
     while(letterIndex !== -1) {
         letterIndex = currentWord.indexOf(letterGuess, startAtIndex);
         console.log ('Found letterGuess at position:', letterIndex);
