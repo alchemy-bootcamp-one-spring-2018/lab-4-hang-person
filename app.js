3aeebee1a47a63f340171d62f82d77a380e149ae
Ryan Khatib <ryan.khatib@gmail.com>
2018-05-03 15:51:47 -0700

    app.js start

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+/* exported gameOn, attempts*/
+
+
+function gameOn() {
+    console.log('game is running');
+
+    var attempts = 6;
+    var answer;
+    var correct = true;
+
+    /* Guess Loop*/
+
+    for(var i = 0; i < 5; i++) {
+        answer = prompt('enter any letter!');
+        console.log(answer);
+    }
+
+
+    
+
+    if(i === 5) {
+        alert('Damn bro, you suck so hard somebody died*/');
+
+    }
+
+
+}
--- words.js
+++ words.js
@@ -1,4 +1,4 @@
-/* 3 Words: Laboratory; Portmanteau; Supercilious *\
-/* Supercilious is the largest word - 12 letter spaces required*\
-/*Stick figure has 6 body parts: head, torso, right arm, left arm, right leg, & left leg*\
-/*End messages: (1) Congrats! You must have finished High School! (2) Damn bro, you suck so hard somebody died*\
+/* 3 Words: Laboratory; Portmanteau; Supercilious */
+/* Supercilious is the largest word - 12 letter spaces required*/
+/* Stick figure has 6 body parts: head, torso, right arm, left arm, right leg, & left leg*/
+/* End messages: (1) Congrats! You must have finished High School! (2) Damn bro, you suck so hard somebody died*/
