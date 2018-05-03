caa67ded87366ef52984d3a6723b2abc8f46c7f8
Carmen V Ramos <carmenvramos@yahoo.com>
2018-05-03 14:09:45 -0700

    Completed RandomSate Func

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
 function randomState() {
     //get a random integer between 0 - stateList.length
     var index = Math.floor(Math.random() * stateList.length);
-    return index;
+    console.log('Selected index', index);
     //use random integer to select from stateList
+    var selectedState = stateList[index];
+    return selectedState;
     //return that state
 }
+    
