f6f50bd86cdfafb5ae3fe23858b775179310385a
Ryan Khatib <ryan.khatib@gmail.com>
2018-05-04 16:38:18 -0700

    input checking against console log

--- app.js
+++ app.js
@@ -18,6 +18,60 @@ function gameOn() {
     var wordArray = gameWord.split('');
     console.log('letters:', wordArray);
 
+    var l = document.getElementById('letter');
+    console.log('input is', l.value);
+
+    for(var i = 0; i < wordArray.length; i++) {
+        if(l.value === wordArray[i]) {
+            console.log('correct!');
+            break;
+
+        }
+    
+        else if(l.value !== wordArray[i]) {
+            console.log('incorrect!');
+
+        }
+        
+        
+        
+        
+        
+        
+        
+        // var array1 = [1, 2, 3];
+
+        // console.log(array1.includes(2));
+        // // expected output: true
+        
+        // var pets = ['cat', 'dog', 'bat'];
+        
+        // console.log(pets.includes('cat'));
+        // // expected output: true
+        
+        // console.log(pets.includes('at'));
+        // // expected output: false
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+// The includes() method determines whether an array includes a certain element, returning true or false as appropriate.
+
+
+
+
+
+
+
+
+
 
     // function showLetter() {
     //     var x = document.getElementById('p');
@@ -43,15 +97,15 @@ function gameOn() {
 
 
 
-    // function loadWord(){
-    //     var index = getRandomIndex(words.length);
-    //     var gameWord = words[index];
-    //     console.log('index is', index);
-    // }
+// function loadWord(){
+//     var index = getRandomIndex(words.length);
+//     var gameWord = words[index];
+//     console.log('index is', index);
+// }
 
-    // function wordArray(){
-    //     gameWord.split();
-    // }
+// function wordArray(){
+//     gameWord.split();
+// }
 
 
 
