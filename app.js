058a1b70097eac2482140831168b49131ec9c970
Ryan Khatib <ryan.khatib@gmail.com>
2018-05-03 17:34:13 -0700

    Thank God for Andrew - random word works

--- app.js
+++ app.js
@@ -1,54 +1,46 @@
 'use strict';
-/* exported gameOn, words attempts*/
+/* exported gameOn, words, attempts*/
 
 /* Words array & individual word properties*/
 
-var words = [
-    'Laboratory',
-    'Supercilious',
-    'Portmanteau',
-];
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomWord(wordArray) {
-    var randomWord = Math.floor(Math.random() * Math.floor(wordArray.length));
-
-    return randomWord;
+function getRandomIndex(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 
 }
 
-console.log(getRandomWord(words));
+// console.log(getRandomIndex(words));
 
 
 
-var laboratory = {
-    characters: 10,
-};
-
-var portmanteau = {
-    characters: 11,
-};
-
-var supercilious = {
-    characters: 12,
-};
-
-function gameOn() {
-    console.log('game is running');
-
-    var attempts = 6;
-    var answer;
-    var correct = true;
-
-    /* Guess Loop*/
-
-    for(var i = 0; i < 5; i++) {
-        answer = prompt('enter any letter!');
-        console.log(answer);
-    }
-
-    if(i === 6 && !correct) {
-        alert('Damn bro, you suck so hard somebody died');
-
-    }
-
+function loadWord(){
+    var index = getRandomIndex(words.length);
+    console.log('index is', index);
+    var gameWord = words[index];
+    console.log('gameWord is', gameWord);
 }
+
+
+
+
+// function gameOn() {
+//     console.log('game is running');
+
+//     var attempts = 6;
+//     var answer;
+//     var correct = true;
+
+//     /* Guess Loop*/
+
+//     for(var i = 0; i < 5; i++) {
+//         answer = prompt('enter any letter!');
+//         console.log(answer);
+//     }
+
+//     if(i === 6 && !correct) {
+//         alert('Damn bro, you suck so hard somebody died');
+
+//     }
+
+// }
--- words.js
+++ words.js
@@ -2,3 +2,10 @@
 /* Supercilious is the largest word - 12 letter spaces required*/
 /* Stick figure has 6 body parts: head, torso, right arm, left arm, right leg, & left leg*/
 /* End messages: (1) Congrats! You must have finished High School! (2) Damn bro, you suck so hard somebody died*/
+
+
+var words = [
+    'Laboratory',
+    'Supercilious',
+    'Portmanteau',
+];
